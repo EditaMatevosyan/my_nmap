e750fe376f905fe713f46878a3daa9840f18e47b
Edith Matevosyan <edithmatevosyan@Edith-Matevosyan-s-MacBook-Air.local>
2026-09-23 17:59:31 +0400

    Add test for single-value range

--- test_scanner.py
+++ test_scanner.py
@@ -11,3 +11,7 @@ def test_port_range():
 
 def test_comma_list():
     assert parse_port_range("22,80,443") == [22, 80, 443]
+
+
+def test_range_of_one():
+    assert parse_port_range("443-443") == [443]
